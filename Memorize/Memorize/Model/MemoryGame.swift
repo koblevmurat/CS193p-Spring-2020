65ba54532e18f54b4dc3ec58c1d8dace719b5097
flin-7 <flin.7@icloud.com>
2020-05-30 12:29:49 -0700

    Lecture 4 Grid enum Optionals

---
+++ Memorize/Memorize/Model/MemoryGame.swift
@@ -0,0 +1,52 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Felix Lin on 5/23/20.
+//  Copyright © 2020 Felix Lin. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> where CardContent: Equatable {
+    var cards: [Card]
+    
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { cards[$0].isFaceUp }.only }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = index == newValue
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(id: pairIndex * 2, content: content))
+            cards.append(Card(id: pairIndex * 2 + 1, content: content))
+        }
+    }
+    
+    mutating func choose(card: Card) {
+        if let chosenIndex = cards.firstIndex(matching: card), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                self.cards[chosenIndex].isFaceUp = true
+            } else {
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
+        }
+    }
+    
+    struct Card: Identifiable {
+        var id: Int
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+    }
+}
--- Memorize/Memorize/Views/EmojiMemoryGameView.swift
+++ Memorize/Memorize/Views/EmojiMemoryGameView.swift
@@ -12,13 +12,12 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        HStack {
-            ForEach(viewModel.cards) { card in
-                CardView(card: card)
-                    .onTapGesture {
-                        self.viewModel.choose(card: card)
-                }
+        Grid(viewModel.cards) { card in
+            CardView(card: card)
+                .onTapGesture {
+                    self.viewModel.choose(card: card)
             }
+            .padding(5)
         }
         .padding()
         .foregroundColor(Color.orange)
@@ -43,8 +42,10 @@ struct CardView: View {
                     .stroke(lineWidth: edgeLineWidth)
                 Text(card.content)
             } else {
-                RoundedRectangle(cornerRadius: cornerRadius)
-                    .fill()
+                if !card.isMatched {
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .fill()
+                }
             }
         }
         .font(Font.system(size: fontSize(for: size)))
