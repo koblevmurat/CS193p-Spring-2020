66db375daf70b89f60d6608072093c741a2f4c4d
Koblev <dev@devs-Mac.local>
2020-06-09 13:49:10 -0700

    lec 7

---
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by dev on 6/9/20.
+//  Copyright © 2020 dev.cs193p.student. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    
+    @ObservedObject var document: EmojiArtDocument
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,9 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by dev on 6/9/20.
+//  Copyright © 2020 dev.cs193p.student. All rights reserved.
+//
+
+import Foundation
